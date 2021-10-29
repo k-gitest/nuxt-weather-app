dba0ff30182fa0fde32cd62d120d68940715e191
k-gitest <adobe_blow@yahoo.co.jp>
2021-10-29 19:10:45 +0000

    storeにgetters,mutations,actionsの処理追加

--- pages/storetest.vue
+++ pages/storetest.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <h1>{{ store_msg }}</h1>
     <StoreX />
+    <ul>
+        <li v-for="user in users">{{ user.name }} {{ user.mail}} {{ user.age }}</li>
+    </ul>
+    <div>
+        <button @click="increment">カウントボタン</button>
+        <button @click="increment_act">actionボタン</button>
+        <p>カウント：{{ count }}</p>
+    </div>
   </div>
 </template>
 
@@ -9,6 +17,7 @@
 import StoreX from '@/components/StoreX.vue'
 
 export default {
+  namespace: true,
   name: 'app',
   data: function(){
     return {
@@ -22,9 +31,29 @@ export default {
   //アクセス名の変更をしてみる
   computed: {
       store_msg: function(){
+          //stateの呼び出し
           return this.$store.state.storetest.message
+      },
+      count: function(){
+          return this.$store.state.storetest.count
+      },
+      users: function(){
+          //gettersの呼び出し
+          return this.$store.getters['storetest/users']
+      },
+  },
+  methods: {
+      increment: function(){
+          //mutationの呼び出し commitを使用する 第二引数のpayloadで値を渡す
+          this.$store.commit('storetest/increment', 1)
+      },
+      
+      increment_act: function(){
+          //actionsの呼び出し dispatchを使用する
+          this.$store.dispatch('storetest/incrementOne')
       }
-  }
+      
+  },
 }
 
 </script>
--- store/storetest.js
+++ store/storetest.js
@@ -1,5 +1,4 @@
-//モジュールモード
-
+//クラシックモード
 /*
 export default new Vuex.Store({
     state: {
@@ -18,6 +17,7 @@ export default new Vuex.Store({
 })
 */
 
+//モジュールモード
 //stateはdataプロパティと同じ様なもの
 export const state = () => ({
     message: 'Hello Vuex',
@@ -25,19 +25,34 @@ export const state = () => ({
             {name: 'John', email:'john@example.com', age:22},
     	    {name: 'Merry', email: 'merry@facebook.com',age:33},
     	    {name: 'Ken', email: 'ken@amazon.com',age:29}
-        ]
+        ],
+    count: 0,//カウント初期値
 })
 
 export const getters = {
-  
+  users: function(state){
+      return state.users.filter(user => user.age < 30 )
+  }
 }
 
 export const mutations = {
-
+    increment: function(state, num){
+        //カウント更新処理 payload用の引数も用意できる
+        state.count = state.count + num
+    },
+    
+    increment_act: function(state){
+        state.count++
+    }
+    
 }
 
 export const actions = {
-
+    incrementOne: function(context){
+        //console.log(context)
+        //mutationsを起動する
+        context.commit('increment_act')
+    }
 }
 
 export const modules = {
