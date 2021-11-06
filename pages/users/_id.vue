9d16697155e21bed6487932391f63bb4538e348f
k-gitest <adobe_blow@yahoo.co.jp>
2021-11-06 18:44:34 +0000

    ユーザー詳細ページ処理追加

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <h1>ユーザID: {{ this.$route.params.id }}</h1>
+        <h1>ユーザID: {{ user.id }}</h1>
         <h2 v-if="user">{{ user.name }}</h2>
+        <ul>
+            <li>{{ user.email }}</li>
+        </ul>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     head(){
         // user名があったらタイトルをユーザー名
@@ -16,6 +21,29 @@ export default {
         }
     },
     
+    async asyncData({params}){ //Contextからparamsを取得
+        const id = params.id //paramsのid取得
+        const url = 'https://jsonplaceholder.typicode.com/users/' 
+        return await axios.get(url + id) //idのデータ取得
+        .then(res  => {
+            //console.log(res.data, res.data)
+            return { user: res.data }
+        })
+        .catch(err => {
+            console.log('error')
+        })
+        
+        /* パラメータで渡す場合
+        const params = {
+            url: 'https://jsonplaceholder.typicode.com/users',
+            id: '',
+            name: '',
+            age: 1,
+        }
+        */
+    },
+    
+    /*
     data(){
         return {
             // $route.param.idで_idが取得できる
@@ -33,13 +61,14 @@ export default {
             ],
         }
     },
+    */
     computed:{
         user(){
             // idが同じならfindで配列内の最初の要素の値を返す
             return this.users.find(user => user.id == this.id)
         } 
     },
-    
+
     mounted() {
       //console.log(this.$route);
     },
