a48be5efadb00caffb660ca6cc8392aebd5de786
k-gitest <adobe_blow@yahoo.co.jp>
2021-11-07 18:54:43 +0000

    fetchでapi読み込み

--- pages/about.vue
+++ pages/about.vue
@@ -2,12 +2,57 @@
     <div>
         <h1>About Page</h1>
         <h2>{{ subtitle }}</h2>
+        
+        <ul>
+            <li v-for="item in items">{{ item }}</li>
+        </ul>
+
+        <ul>
+            <li v-for="weather in weathers">{{ weather }}</li>
+        </ul>
+        <button @click="are_write(`overview_forecast/130000.json`)">東京</button>
+        <button @click="are_write(`overview_forecast/140000.json`)">横浜</button>
+
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default{
   layout: 'original',
+  data(){
+      return{
+          subtitle: 'この文書はNuxt.jsでの気象庁のデータについて説明しています。',
+          weathers: [],
+          url: 'https://www.jma.go.jp/bosai/forecast/data/',
+          area: 'overview_forecast/130000.json'
+      }
+  },
+
+  // asyncDataではthisは不可、propsは可能
+  async asyncData(){
+      const url = 'https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json'
+      
+      return await axios.get(url)
+      .then(res => {
+          //console.log(res.data)
+          return {items: res.data}
+          })
+          
+  },
+  
+  // fetchではthisが可能
+  async fetch(){
+      //const url = 'https://www.jma.go.jp/bosai/forecast/data/'
+      this.weathers = await fetch(this.url + this.area).then(res => res.json())
+  },
+  methods: {
+      are_write: async function(area){
+          this.area = area
+          this.weathers = await fetch(this.url + this.area).then(res => res.json())
+      },
+  },
   
   // headプロパティでtitleやmetaなどを設定できる
   head(){
@@ -23,10 +68,8 @@ export default{
       }
   },
   
-  data(){
-      return{
-          subtitle: 'この文書はNuxt.jsの基本的な使用方法について説明しています。'
-      }
-  },
+  
+
+  
 }
 </script>
