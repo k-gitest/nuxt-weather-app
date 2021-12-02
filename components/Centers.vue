098d7d55f48e98aa2e3326f7c85e0229dbc7ca24
k-gitest <adobe_blow@yahoo.co.jp>
2021-12-02 19:56:22 +0000

    cssファイル追加、モーダルスタイル追加

--- components/Centers.vue
+++ components/Centers.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
     <template v-for="center in centers">
-    <ul>
-      <li>{{ center.name }}</li>
-      <li v-for="item in center.children">
+    <h2>{{ center.name }}</h2>
+    <ul class="list-group list-group-horizontal row">
+      <li class="col-4" v-for="item in center.children">
         <nuxt-link :to="`forecast/${ item }`">
           {{ offices[item].name }}
           
@@ -33,40 +33,74 @@
         </nuxt-link><br>
 
         <button class="btn btn-success" @click="areaMenu(item)">地域</button>
-        <div v-if="isActive.indexOf(item) >= 0">
-          <div class="row">
-            <template v-for="office_child in offices[item].children">
-              <template v-for="class10_child in class10s[office_child].children">
-                
-              <div class="col-6">
-                <h3 class="h5">{{ class15s[class10_child].name }}</h3>
-                <ul class="list-group list-group-horizontal row">
-                  <template v-for="class15_child in class15s[class10_child].children">
-                  <li class="col-6">
-                    
-                  <nuxt-link :to="`forecast/${ item }?area_detail=${class15s[class10_child].parent}`">
-                    {{ class20s[class15_child].name }}
-                  </nuxt-link><br>
-                
-                  </li>
-                  </template>
-                </ul>
-              </div>
-
+        
+        <div v-if="isActive.indexOf(item) >= 0" class="areamodal-back">
+          <div class="container areamodal">
+            <div class="row areamodal-content">
+              <template v-for="office_child in offices[item].children">
+                <template v-for="class10_child in class10s[office_child].children">
+                  
+                <div class="col-6">
+                  <h3 class="h5">{{ class15s[class10_child].name }}</h3>
+                  <ul class="list-group list-group-horizontal row">
+                    <template v-for="class15_child in class15s[class10_child].children">
+                    <li class="col-6">
+                      
+                    <nuxt-link :to="`forecast/${ item }?area_detail=${class15s[class10_child].parent}`">
+                      {{ class20s[class15_child].name }}
+                    </nuxt-link><br>
+                  
+                    </li>
+                    </template>
+                  </ul>
+                </div>
+  
+                </template>
               </template>
-            </template>
+            </div>
+            <div @click="areaMenu(item)" class="btn btn-success">閉じる</div>
           </div>
+          
+          
+          
         </div>
+        
       </li>
     </ul>
+    
+    <!--
+    <div v-for="item in center.children" class="areamodal">
+      <div v-if="isActive.indexOf(item) >= 0">
+        <div class="row">
+          <template v-for="office_child in offices[item].children">
+            <template v-for="class10_child in class10s[office_child].children">
+              
+            <div class="col-6">
+              <h3 class="h5">{{ class15s[class10_child].name }}</h3>
+              <ul class="list-group list-group-horizontal row">
+                <template v-for="class15_child in class15s[class10_child].children">
+                <li class="col-6">
+                  
+                <nuxt-link :to="`forecast/${ item }?area_detail=${class15s[class10_child].parent}`">
+                  {{ class20s[class15_child].name }}
+                </nuxt-link><br>
+              
+                </li>
+                </template>
+              </ul>
+            </div>
+  
+            </template>
+          </template>
+        </div>
+      </div>
+    </div>
+    -->
+    
     </template>
   </div>
 </template>
-<style>
-  .areamodal{
-    display:none;
-  }
-</style>
+
 <script>
 import axios from 'axios'
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // グローバルCSS
   css: [
+    "@/assets/css/style.css"
   ],
 
   // ページをレンダリングする前に実行するプラグイン
